c5559a957a568624109f144c50d2e6032f3fb019
Prariyavit Tachakitmatetumrong <prariyavit.ta@gmail.com>
2025-12-14 23:59:43 +0700

    chore: rename usecase

--- internal/interfaces/http/routes/public_routes.go
+++ internal/interfaces/http/routes/public_routes.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	accUseCase "github.com/FrostBitzX/smart-task-ai/internal/application/account/usecase"
+	accSignUpUC "github.com/FrostBitzX/smart-task-ai/internal/application/account/usecase"
 	accDomain "github.com/FrostBitzX/smart-task-ai/internal/domain/accounts/service"
 	"github.com/FrostBitzX/smart-task-ai/internal/infrastructure/logger"
 	repo "github.com/FrostBitzX/smart-task-ai/internal/infrastructure/persistence"
@@ -16,8 +16,8 @@ func RegisterPublicRoutes(app fiber.Router, db *gorm.DB, log logger.Logger) {
 
 	accountRepository := repo.NewAccountRepository(db)
 	accountService := accDomain.NewAccountService(accountRepository)
-	accountUsecase := accUseCase.NewAccountUseCase(accountService, log)
-	accountHandler := accHandler.NewAccountHandler(accountUsecase, log)
+	accountSignUpUC := accSignUpUC.NewAccountUseCase(accountService, log)
+	accountHandler := accHandler.NewAccountHandler(accountSignUpUC, log)
 
 	api.Post("/signup", accountHandler.CreateAccount)
 }
