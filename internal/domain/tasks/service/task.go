34e282ec93d9b37c368df4cc022b092822699c5f
FrostBitz_X <f_yossawaj@hotmail.com>
2026-01-02 12:03:46 +0700

    feat: Implement create task api endpoints

---
+++ internal/domain/tasks/service/task.go
@@ -0,0 +1,65 @@
+package service
+
+import (
+	"context"
+	"time"
+
+	"github.com/FrostBitzX/smart-task-ai/internal/application/task"
+	"github.com/FrostBitzX/smart-task-ai/internal/errors/apperrors"
+	"github.com/google/uuid"
+
+	"github.com/FrostBitzX/smart-task-ai/internal/domain/tasks"
+	"github.com/FrostBitzX/smart-task-ai/internal/domain/tasks/entity"
+)
+
+type TaskService struct {
+	repo tasks.TaskRepository
+}
+
+func NewTaskService(repo tasks.TaskRepository) *TaskService {
+	return &TaskService{repo: repo}
+}
+
+func (s *TaskService) CreateTask(ctx context.Context, projectID uuid.UUID, req *task.CreateTaskRequest) (*entity.Task, error) {
+	if req == nil {
+		return nil, apperrors.NewBadRequestError("invalid request body", "INVALID_REQUEST", nil)
+	}
+
+	// create domain entity
+	now := time.Now()
+	task := &entity.Task{
+		ID:            uuid.New(),
+		ProjectID:     projectID,
+		Name:          req.Name,
+		Priority:      req.Priority,
+		StartDateTime: req.StartDateTime,
+		EndDateTime:   req.EndDateTime,
+		Status:        "todo",
+		CreatedAt:     now,
+		UpdatedAt:     now,
+	}
+
+	if req.Description != nil {
+		task.Description = req.Description
+	}
+
+	if req.Location != nil {
+		task.Location = req.Location
+	}
+
+	if req.RecurringDays != nil {
+		task.RecurringDays = req.RecurringDays
+	}
+
+	if req.RecurringUntil != nil {
+		task.RecurringUntil = req.RecurringUntil
+	}
+
+	// persist account to database
+	err := s.repo.CreateTask(ctx, task)
+	if err != nil {
+		return nil, apperrors.NewInternalServerError("failed to create task", "CREATE_TASK_ERROR", err)
+	}
+
+	return task, nil
+}
